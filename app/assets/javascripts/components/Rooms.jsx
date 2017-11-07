02b3c60b8a9782b2ce74ab646a58f1c4dfe740fe
h7kayama <0hideki0@gmail.com>
2017-11-07 23:41:14 +0900

    React導入

--- app/assets/javascripts/components/Rooms.jsx
+++ app/assets/javascripts/components/Rooms.jsx
@@ -1,7 +1,56 @@
-var Rooms = React.createClass({
-  render: function() {
+let Channel;
+
+let Rooms = React.createClass({
+  getInitialState(){
+    console.log("call Rooms#getInitialState")
+
+    _this = this
+    Channel = App.cable.subscriptions.create('RoomChannel', {
+      connected() { console.log("call Rooms#connected") },
+      disconnected() { console.log("call Rooms#disconnected") },
+      received(data) {
+        console.log("call Rooms#received")
+        _this.updateMessages(data['message'])
+      },
+      speak(message) {
+        this.perform('speak', {message: message})
+      }
+    })
+
+    return {messages: this.props.messages, content: ""}
+  },
+
+  speak(e) {
+    console.log("call Rooms#speak")
+    Channel.speak(this.state.content)
+    this.setState({content: ""})
+    e.preventDefault()
+  },
+
+  updateMessages(message) {
+    console.log("call Rooms#updateMessages")
+    this.setState({messages: [...this.state.messages, message]})
+  },
+
+  changeContent(e) {
+    console.log("call Rooms#changeContent")
+    this.setState({content: e.target.value})
+  },
+
+  render() {
+    console.log("call Rooms#render")
+    let {messages,content} = this.state
     return (
-      <h1>Hello {this.props.name}!</h1>
+      <div>
+        <h1>Chat room</h1>
+        {messages.map((message,index)=>{
+          return <p key={index}>{message.content}</p>
+        })}
+        <form onSubmit={this.speak}>
+          <label>Say something:</label>
+          <input type="text" value={content} onChange={this.changeContent} />
+        </form>
+      </div>
     )
   }
 });
